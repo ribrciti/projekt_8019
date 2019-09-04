62f1cc94f5b872b5eb2b0c49b08815491f3a85d2
ribrciti <rbciti@msn.com>
2019-09-04 12:28:56 -0700

    Implemented confirmations and registrations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmations: 'confirmations'
+  }
+
   root to: "home#index"
 end
